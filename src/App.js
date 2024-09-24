f58ffb020655d5eabca7c010dd73c28ed98b7bbd
geonwoo1098 <geonwoo1098@gmail.com>
2024-09-24 13:28:45 +0900

    page create

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Presale from "./pages/Presale";
 import News from "./pages/News";
+import Not from "./pages/Not";
+import Channel from "./pages/Channel";
+import Search from "./pages/Search";
+import Today from "./pages/Today";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path='/today' element={<Today />} />
         <Route path="/news" element={<News />} />
         <Route path="/presale" element={<Presale />} />
+        <Route path='/channel/:channelId' element={<Channel />} />
+        <Route path='/search/:searchId' element={<Search />} />
+        <Route path="/*" element={<Not />} />
       </Routes>
     </BrowserRouter>
   );
